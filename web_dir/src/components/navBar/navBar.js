9621c505c2db5b4f7e46d98ee4c42ab876761a4b
trisaleung <trisaleung1812@gmail.com>
2021-03-27 22:12:12 -0700

    video page added

--- web_dir/src/components/navBar/navBar.js
+++ web_dir/src/components/navBar/navBar.js
@@ -15,23 +15,8 @@ const Navbar = () => {
         <Bars />
   
         <NavMenu>
-          <NavLink to='/about' activeStyle>
-            About
-          </NavLink>
-          <NavLink to='/events' activeStyle>
-            Events
-          </NavLink>
-          <NavLink to='/annual' activeStyle>
-            Annual Report
-          </NavLink>
-          <NavLink to='/team' activeStyle>
-            Teams
-          </NavLink>
-          <NavLink to='/blogs' activeStyle>
-            Blogs
-          </NavLink>
-          <NavLink to='/sign-up' activeStyle>
-            Sign Up
+          <NavLink to='/home' activeStyle>
+            NAME
           </NavLink>
           {/* Second Nav */}
           {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
---
+++ web_dir/src/pages/video.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+/* We simply can use an array and loop and print each user */
+const VideoPage = () => {
+  return (
+    <div>
+      <div className="Videos">
+
+      </div>
+      <div className="Messages">
+
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
